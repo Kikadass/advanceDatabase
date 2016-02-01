ff3f794d70486cf1aca934dcd5a47a35ce2eb0fb
Kikadass <kikadass@gmail.com>
2016-02-01 16:55:38 +0000

    formated

--- src/Crime.py
+++ src/Crime.py
@@ -1,4 +1,35 @@
-print("lol")
+from xml.dom.minidom import *
 
-print("hello")
+
+def format(doc):
+    return  parseString(doc.toprettyxml())
+
+def store():
+    myDoc = Document()
+
+    newElement = myDoc.createElement("squid")
+
+    text = myDoc.createTextNode("Some Text")
+    newElement.appendChild(text)
+
+    environment = myDoc.createElement("environment")
+    newElement.appendChild(environment)
+
+
+    for i in myDoc.getElementsByTagName("fish"):
+        environment = myDoc.createElement("environment")
+        i.appendChild(environment)
+        print i
+
+    myDoc.appendChild(newElement)
+
+    myDoc = format(myDoc)
+
+    out = open("../XML/crime.xml", 'w')
+    myDoc.writexml(out)
+    out.flush()
+    out.close()
+
+
+store()
 
